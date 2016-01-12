070bb365ef924b339b4861d8628b5d8482c91bbd
Janos Laube <janos.dev@gmail.com>
2016-01-12 21:25:38 +0100

    silence musl build
    Signed-off-by: Janos Laube <janos.dev@gmail.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WD="`pwd`"
-cd musl && CFLAGS=-O2 ./configure --prefix="$WD/local" --enable-shared=no > /dev/null && cd .. && make -j2 -C musl && make -s -C musl install
+cd musl && CFLAGS=-O2 ./configure --prefix="$WD/local" --enable-shared=no > /dev/null && cd .. && make -s -j2 -C musl && make -s -C musl install
 
 gcc -specs=./local/lib/musl-gcc.specs -static -O2 -fomit-frame-pointer -o test main.c
 
